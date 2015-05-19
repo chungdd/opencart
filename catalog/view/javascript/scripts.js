da8a842470f66f3cc134d6fb4d0f70752ba6d49f
Chung Do Duc <chungdd307@gmail.com>
2015-05-19 23:27:18 +0700

    update

--- catalog/view/javascript/scripts.js
+++ catalog/view/javascript/scripts.js
@@ -1,21 +1,35 @@
 $(function() {
 	// configure how the popup window should look
-	$("#shippingManager").dialog({ autoOpen: false, width: 835, height: 600, draggable: false, resizable: false, modal: true,
+	/*$("#shippingManager").dialog({ autoOpen: false, width: 835, height: 600, draggable: false, resizable: false, modal: true,
 		closeOnEscape: false,
 		// Scrollbar fix  for ie
 		open: function(event, ui){$("body").css("overflow","hidden");$(".ui-widget-overlay").css("width","100%"); },
 		close: function(event, ui){$("body").css("overflow","auto"); } 				
-	});
+	});*/
 });
 
 function loadShippingManager() {
 	// put the page that contains a form with all the data to be submitted to the Shipping Manager as src of the iframe in the popup
-	//$("#shippingManager").find("iframe").attr("src", "./loadShippingManager.php");				
-	$("#shippingManager").find("iframe").attr("src", "loadShippingManager.php");
-	$("#result").remove();				
-	
+	//$("#shippingManager").find("iframe").attr("src", "./loadShippingManager.php");
+    var params = '?';
+    params += 'first_name=' + get_input_value('input-shipping-firstname');
+    params += '&last_name=' + get_input_value('input-shipping-lastname');
+    params += '&company=' + get_input_value('input-shipping-company');
+    params += '&address=' + get_input_value('input-shipping-address-1');
+    params += '&city=' + get_input_value('input-shipping-city');
+    params += '&email=' + get_input_value('input-payment-email');
+    params += '&phone=' + get_input_value('input-payment-telephone');
+    			
+	$("#shippingManager").find("iframe").attr("src", "loadShippingManager.php" + params);
+	$("#result").remove();
+    
 	// open the popup
-	$("#shippingManager").dialog();				
+	$("#shippingManager").dialog({width: 835, height: 600, draggable: false, resizable: false});
+    				
+}
+
+function get_input_value(elm_id) {
+    return $('#' + elm_id).val();
 }
 
 function closeShippingManager() {				
--- loadShippingManager.php
+++ loadShippingManager.php
@@ -1,61 +1,80 @@
 <html>
-	<head>
-		<script src="catalog/view/javascript/jquery/jquery-2.1.1.min.js" type="text/javascript"></script>
+    <head>
+        <script src="catalog/view/javascript/jquery/jquery-2.1.1.min.js" type="text/javascript"></script>
 				
-		<script type="text/javascript">
-			$(function() {
-				// immediately submit the form, containing all the parameters for the Shipping Manager
-				$("#dataForm").submit();
-			});
+        <script type="text/javascript">
+            $(function() {
+                // immediately submit the form, containing all the parameters for the Shipping Manager
+                passInputValue('customerFirstName', 'first_name');
+                passInputValue('customerLastName', 'last_name');
+                passInputValue('customerCompany', 'company');
+                passInputValue('customerStreet', 'address');
+                passInputValue('customerCity', 'city');
+                passInputValue('customerEmail', 'email');
+                passInputValue('customerPhoneNumber', 'phone');
+                
+                $("#dataForm").submit();
+    		});
+            
+            function passInputValue(elm_name, param_name) {
+                $('input[name="' + elm_name + '"]').val(getUrlParameter(param_name));
+            }
+            
+            function getUrlParameter(sParam) {
+                var sPageURL = window.location.search.substring(1);
+                var sURLVariables = sPageURL.split('&');
+                for (var i = 0; i < sURLVariables.length; i++) {
+                    var sParameterName = sURLVariables[i].split('=');
+                    if (sParameterName[0] == sParam) {
+                        return sParameterName[1];
+                    }
+                }
+            }      
 		</script>
-	</head>		
+	</head>
+    	
 	<body>
+        <?php
+        	$accountId = '107413';
+        	$passphrase = 'DEMO_SHM';
+        	$orderReference = rand(100,999);
+        	$returnURL = 'http://localhost/opencart/closeShippingManager.php';
+        ?>	
 			
-		<?php
-			
-			$accountId = '107413';
-			$passphrase = 'DEMO_SHM';
-			$orderReference = rand(100,999);
-			$returnURL = 'http://localhost/bpost/closeShippingManager.php';
-			
-		?>	
-			
-		<form style="display: none" id="dataForm" method="POST" action="https://shippingmanager.bpost.be/ShmFrontEnd/start">
-			<input type="submit" value="Confirm button Shipping Manager" />
-			<input type="hidden" name="lang" value="EN"/>
-			<input type="hidden" name="accountId" value="<?php echo $accountId; ?>"/>
-			<input type="hidden" name="action" value="START"/>
-			<input type="hidden" name="orderReference" value="<?php echo $orderReference; ?>"/>
-			<input type="hidden" name="orderTotalPrice" value="13500"/>
-			<input type="hidden" name="customerFirstName" value="John"/>
-			<input type="hidden" id="orderWeight" name="orderWeight" value="4500" />
-			<input type="hidden" id="confirmUrl" name="confirmUrl" value="<?php echo $returnURL; ?>" />
-			<!--<input type="hidden" name="deliveryMethodOverrides" value="bpack BUSINESS|VISIBLE|0" />
-			<input type="hidden" name="deliveryMethodOverrides" value="Parcels depot|VISIBLE" />
-			<input type="hidden" name="deliveryMethodOverrides" value="Pugo|VISIBLE" />
-			<input type="hidden" name="deliveryMethodOverrides" value="Regular|VISIBLE" />
-			<input type="hidden" name="deliveryMethodOverrides" value="bpack BUSINESS|VISIBLE" />
-			<input type="hidden" name="deliveryMethodOverrides" value="bpack EXPRESS|VISIBLE" />-->
-			<input type="hidden" name="customerLastName" value="Doe"/>
-			<input type="hidden" name="customerStreet" value="Munt"/>
-			<input type="hidden" name="customerStreetNumber" value="1"/>
-			<input type="hidden" name="orderLine" value="Some product info|1"/>
-			<input type="hidden" name="orderLine" value="Some other product info|2"/>
-			<input type="hidden" name="customerBox" value=""/>
-			<input type="hidden" name="customerCity" value="Brussel"/>
-			<input type="hidden" name="customerPostalCode" value="1000"/>
-			<input type="hidden" name="customerEmail" value="john.doe@mail.be"/>
-			<input type="hidden" name="customerPhoneNumber" value="0032499123456"/>
-			<input type="hidden" name="customerCountry" value="BE"/>
+        <form style="display: none" id="dataForm" method="POST" action="https://shippingmanager.bpost.be/ShmFrontEnd/start">
+            <input type="submit" value="Confirm button Shipping Manager" />
+            <input type="hidden" name="lang" value="EN"/>
+            <input type="hidden" name="accountId" value="<?php echo $accountId; ?>"/>
+            <input type="hidden" name="action" value="START"/>
+            <input type="hidden" name="orderReference" value="<?php echo $orderReference; ?>"/>
+            <input type="hidden" name="orderTotalPrice" value="13500"/>
+            <input type="hidden" name="customerFirstName" value=""/>
+            <input type="hidden" id="orderWeight" name="orderWeight" value="4500" />
+            <input type="hidden" id="confirmUrl" name="confirmUrl" value="<?php echo $returnURL; ?>" />
+            <!--<input type="hidden" name="deliveryMethodOverrides" value="bpack BUSINESS|VISIBLE|0" />
+            <input type="hidden" name="deliveryMethodOverrides" value="Parcels depot|VISIBLE" />
+            <input type="hidden" name="deliveryMethodOverrides" value="Pugo|VISIBLE" />
+            <input type="hidden" name="deliveryMethodOverrides" value="Regular|VISIBLE" />
+            <input type="hidden" name="deliveryMethodOverrides" value="bpack BUSINESS|VISIBLE" />
+            <input type="hidden" name="deliveryMethodOverrides" value="bpack EXPRESS|VISIBLE" />-->
+            <input type="hidden" name="customerLastName" value=""/>
+            <input type="hidden" name="customerCompany" value=""/>
+            <input type="hidden" name="customerStreet" value=""/>
+            <input type="hidden" name="customerStreetNumber" value=""/>
+            <input type="hidden" name="orderLine" value="Some product info|1"/>
+            <input type="hidden" name="orderLine" value="Some other product info|2"/>
+            <input type="hidden" name="customerBox" value=""/>
+            <input type="hidden" name="customerCity" value=""/>
+            <input type="hidden" name="customerPostalCode" value=""/>
+            <input type="hidden" name="customerEmail" value=""/>
+            <input type="hidden" name="customerPhoneNumber" value=""/>
+            <input type="hidden" name="customerCountry" value="BE"/>
 		
 		<?php
 			$hash = hash("sha256", "accountId=".$accountId."&action=START&customerCountry=BE&orderReference=".$orderReference."&orderWeight=4500&".$passphrase);
-			//$hash = hash("sha256", "accountId=".$accountId."&action=START&customerCountry=BE&deliveryMethodOverrides=bpack BUSINESS|VISIBLE|0&orderReference=".$orderReference."&orderWeight=4500&".$passphrase);
-			//$hash = hash("sha256", "accountId=".$accountId."&action=START&customerCountry=BE&deliveryMethodOverrides=Parcels depot|VISIBLE&deliveryMethodOverrides=Pugo|VISIBLE&deliveryMethodOverrides=Regular|VISIBLE&deliveryMethodOverrides=bpack BUSINESS|VISIBLE&deliveryMethodOverrides=bpack EXPRESS|VISIBLE&orderReference=".$orderReference."&orderWeight=45000&".$passphrase);
 		?>
 		
-		<input type="hidden" name="checksum" value="<?php echo $hash;?>"/>		
-		</form>
-		
+            <input type="hidden" name="checksum" value="<?php echo $hash;?>"/>		
+        </form>
 	</body>
 </html>
